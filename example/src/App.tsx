b31315e982e0d59e595d8301804f605acaee1e62
aghazal <a.ghazal@zenhr.com>
2024-03-07 16:53:44 +0300

    initial commit

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'test-module'
+import 'test-module/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
